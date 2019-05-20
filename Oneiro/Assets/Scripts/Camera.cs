3687737854cdc132201d3a1520fdaa3bd5d065d2
Mark <335494426@qq.com>
2019-05-20 16:00:23 +0100

    Player movement
    player movement

---
+++ Oneiro/Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = target.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = target.position - offset; 
+    }
+}
