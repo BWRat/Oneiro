d9f32a047c06c13740b6ae9a9f8042a4b6a5b49e
Blake Ratliff <blake.ratliff.97@gmail.com>
2019-05-28 16:50:18 +0100

    New animations
    Imported new jump animations from Alice

--- Oneiro/Assets/Scripts/PlayerControl.cs
+++ Oneiro/Assets/Scripts/PlayerControl.cs
@@ -13,26 +13,26 @@ public class PlayerControl : MonoBehaviour
     public float jumpSpeed;
     public float gravity;
     public bool jumpAllowed;
-    private Vector3 velocity;
+    private Vector3 m_velocity;
 
     //Rotation
-    private Vector3 forwardRotation = new Vector3(0, 270, 0);
-    private Vector3 backwardRotation = new Vector3(0, 90, 0);
+    private Vector3 m_forwardRotation = new Vector3(0, 270, 0);
+    private Vector3 m_backwardRotation = new Vector3(0, 90, 0);
 
     //Components
-    private Rigidbody rb;
-    private Animator anim;
+    private Rigidbody m_rb;
+    private Animator m_anim;
 
     [Header("Held objects:")]
     public float grabDistance;
-    private GameObject heldObject;
+    private GameObject m_heldObject;
     #endregion
 
     void Awake()
     {
         //Initialise local components
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
+        m_rb = GetComponent<Rigidbody>();
+        m_anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -41,52 +41,52 @@ public class PlayerControl : MonoBehaviour
 
         Move(moveAxis);
 
-        if (Input.GetAxis("Right_Trigger") > 0 && heldObject == null)
+        if (Input.GetAxis("Right_Trigger") > 0 && m_heldObject == null)
         {
             //Check for push/pull objects in front of player
             print("Pulled right trigger.");
             PushPull();
         }
-        else if (heldObject != null && Input.GetAxis("Right_Trigger") <= 0)
+        else if (m_heldObject != null && Input.GetAxis("Right_Trigger") <= 0)
         {
             //Release object
-            heldObject.transform.parent = null;
-            heldObject = null;
+            m_heldObject.transform.parent = null;
+            m_heldObject = null;
 
-            if (anim.GetBool("Grabbing") == true)
-                anim.SetBool("Grabbing", false);
+            if (m_anim.GetBool("Grabbing") == true)
+                m_anim.SetBool("Grabbing", false);
         }
     }
 
     private void Move(float input)
     {
         print(input);
-        if (input > .5f)
+        if (input > .25f)
         {
             //Walk toward right of screen using root motion
-            transform.rotation = Quaternion.Euler(forwardRotation);
-            anim.SetBool("Walking", true);
-            anim.speed = moveRate * input;
+            transform.rotation = Quaternion.Euler(m_forwardRotation);
+            m_anim.SetBool("Walking", true);
+            m_anim.speed = moveRate * input;
         }
         else if (input < -.25f)
         {
             //Walk toward left of screen using root motion
-            transform.rotation = Quaternion.Euler(backwardRotation);
-            anim.SetBool("Walking", true);
-            anim.speed = Mathf.Abs(moveRate * input);
+            transform.rotation = Quaternion.Euler(m_backwardRotation);
+            m_anim.SetBool("Walking", true);
+            m_anim.speed = Mathf.Abs(moveRate * input);
         }
-        else if (anim.GetBool("Walking") == true)
+        else if (m_anim.GetBool("Walking") == true)
         {
             //Stop walking
-            anim.SetBool("Walking", false);
-            anim.speed = 1;
+            m_anim.SetBool("Walking", false);
+            m_anim.speed = 1;
         }
 
         //Jump
         if (Input.GetButtonDown("A-Button") && jumpAllowed)
         {
             print("Jumping");
-            anim.SetTrigger("Jump");
+            m_anim.SetTrigger("Jump");
             jumpAllowed = false;
             //rb.velocity += jumpSpeed * Vector3.up;
         }
@@ -109,25 +109,25 @@ public class PlayerControl : MonoBehaviour
 
             if (hit.transform.tag == "PushPull")
             {
-                if (heldObject == null)
+                if (m_heldObject == null)
                 {
-                    anim.SetTrigger("Grab");
-                    anim.SetBool("Grabbing", true);
+                    m_anim.SetTrigger("Grab");
+                    m_anim.SetBool("Grabbing", true);
                 }
 
                 //Grab the object if it is a push/pull object
-                heldObject = hit.transform.gameObject;
-                heldObject.transform.parent = transform;
+                m_heldObject = hit.transform.gameObject;
+                m_heldObject.transform.parent = transform;
             }
-            else if (anim.GetBool("Grabbing") == true)
-                anim.SetBool("Grabbing", false);
+            else if (m_anim.GetBool("Grabbing") == true)
+                m_anim.SetBool("Grabbing", false);
         }
         else
         {
             Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward) * grabDistance, Color.white);
 
-            if (anim.GetBool("Grabbing") == true)
-                anim.SetBool("Grabbing", false);
+            if (m_anim.GetBool("Grabbing") == true)
+                m_anim.SetBool("Grabbing", false);
         }
     }
 }
